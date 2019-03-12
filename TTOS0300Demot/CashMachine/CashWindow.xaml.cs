fc1d923077ce830076866ea023ae405ed6d551e1
M3156 <m3156@student.jamk.fi>
2019-03-12 10:35:29 +0200

    cash machine demo

---
+++ TTOS0300Demot/CashMachine/CashWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CashMachine
+{
+
+    /// <summary>
+    /// Interaction logic for CashWindow.xaml
+    /// </summary>
+    public partial class CashWindow : Window
+    {
+        //PROPERTIES
+        public float Mount { get; set; }
+        //CONSTRUCTORS
+        public CashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            //näytetään UIssa maksettava summa
+            txbTotal.Text = "Yhteensä: " + Mount.ToString("C");
+        }
+
+        private void txtMoney_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //muutetaan teksti luvuksi ja lasketaan takaisin annettava rahamäärä
+            float money = 0;
+            if (float.TryParse(txtMoney.Text, out money))
+                txbBack.Text = "Takaisin: " + (money - Mount).ToString("C");
+        }
+
+        private void btnPaid_Click(object sender, RoutedEventArgs e)
+        {
+            //1. tallennetaan myynti päivämyyntiin
+            //2. kerrotaan pääikkunalle että lista tyhjennetään
+            //3. suljetaan ikkuna
+            this.Close();
+        }
+        //NETHODS
+        //EVENT HANDLERS
+    }
+}
---
+++ TTOS0300Demot/CashMachine/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashMachine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private float summa = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //käsitellään kaikki ostosten tapahtuman samalla tavalla
+            //kastataan sender Button-tyyppiseksi olioksi
+            Button pressed = (Button)sender;
+            //lisätään ostos listaan
+            lstItems.Items.Add(pressed.Content);
+            //lasketaan ostosten kokonaissumma
+            
+            switch (pressed.Content)
+            {
+                case "Kahvi 1,50€":
+                    summa += 1.50F;
+                    break;
+                case "Tee 1,10€":
+                    summa += 1.10F;
+                    break;
+                case "Pulla 1,60€":
+                    summa += 1.60F;
+                    break;
+                case "Sämpylä 2,50€":
+                    summa += 2.50F;
+                    break;
+                case "Suklaa 1,30€":
+                    summa += 1.30F;
+                    break;
+            }
+
+            txbTotal.Text = "Yhteensä: " + summa.ToString("C"); // "C" on valuutta (currency)
+            
+        }
+
+        private void btnPaidWithCash_Click(object sender, RoutedEventArgs e)
+        {
+            // avataan käteismaksu-ikkuna
+            CashWindow cashWindow = new CashWindow();
+            cashWindow.Mount = summa;
+            cashWindow.ShowDialog();
+            //tyhjennetään lista ja nollataan summa
+            lstItems.Items.Clear();
+            summa = 0;
+            txbTotal.Text = "Yhteensä: " + summa.ToString("C");
+        }
+    }
+}
